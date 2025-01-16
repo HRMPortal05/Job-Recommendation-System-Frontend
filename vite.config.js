46188684de6f68f4685c4c16ea59f8589105178d
HRMPortal05 <miravsavaliya2004@gmail.com>
2025-01-16 22:29:53 +0530

    Progressive Web App Configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
   server: {
     host: "127.0.0.1",
     port: 5173,
